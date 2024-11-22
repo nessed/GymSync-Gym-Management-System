647036322126339908f47d761bc5d714ef217a58
nessed <ali.abid44444@gmail.com>
2024-11-23 00:36:05 +0500

    new code

--- main.cpp
+++ main.cpp
@@ -1,246 +1,198 @@
+#include <SFML/Graphics.hpp>
 #include <iostream>
-#include <string>
 #include <fstream>
-#define _TIME_ __TIME__  
-#define _DATE_ __DATE__  
+#include <string>
 using namespace std;
 
-void memberCheckIn(string memberid)
-
-{
-    ofstream file2("members_check_in_log.txt", ios::app);
-
-    file2 << memberid << "  " << _TIME_ << "  " << _DATE_ << '\n';
-
-    file2.close();
-}
-
-void memberCheckOut(string memberid)
-{
-    ofstream file3("members_check_out_log.txt", ios::app);
-
-    file3 << memberid << "  " << _DATE_ << "  " << _TIME_ << '\n';
-
-    file3.close();
-}
-
-string generateMemberId()
-{
+// Function to generate new Member ID
+string generateMemberId() {
     string memberid;
-    
     ifstream file("members.txt");
-    
-    if (file.peek() == EOF)
-    {
+
+    if (file.peek() == EOF) {
         memberid = "10001";
-    }
-    else
-    {
+    } else {
         string line;
-        while (getline(file, line))
-        {
+        while (getline(file, line)) {
             memberid = line;
         }
-
         memberid = to_string(stoi(memberid) + 1);
     }
     file.close();
-
     return memberid;
 }
 
-void searchMember(string memberid)
-{
-    string name, contactno, membershipstartdate, membershiptype, activitystatus;
-    ifstream file("members.txt");
-    string currentline;
-    do {
-        getline(file, name);
-        getline(file, contactno);
-        getline(file, membershipstartdate);
-        getline(file, membershiptype);
-        getline(file, activitystatus);
-        getline(file, currentline);
-    } while (currentline != memberid);
-    file.close();
-
-    cout << "Name: " << name << endl;
-    cout << "Contact Number: " << contactno << endl;
-    cout << "Membership Start Date: " << membershipstartdate << endl;
-    cout << "Membership Type: " << membershiptype << endl;
-    cout << "Activity Status: " << activitystatus << endl;
-}
-
-void newRegistration()
-{
+// Function to register a new member
+void newRegistration(sf::RenderWindow &window, sf::Font &font) {
     string name, contactno, membershipstartdate, membershiptype, memberid;
+    bool registrationComplete = false;
 
-    cout << "Enter your name: ";
-    cin >> name;
-    cout << "Enter your contact number: ";
-    cin >> contactno;
-    membershipstartdate = _DATE_;
-    cout << "The list of membership types and their corresponding monthly fee is mentioned below: " << endl;
-    cout << "   1. Silver - Gym only ($10)" << endl;
-    cout << "   2. Silver+ - Gym and Cardio only ($15.5)" << endl;
-    cout << "   3. Gold - Gym only with a trainer ($30)" << endl;
-    cout << "   4. Platinum - Gym, Cardio and a trainer ($32)" << endl;
-    
-    int userinput; 
-    do {
-    if ((userinput != 1) && (userinput != 2) && (userinput != 3) && (userinput != 4))
-    {
-        cout << "Enter number ranging from 1-4 only";
-    }  
-    cout << "Enter the number of membership you want (For Silver, press 1; For Silver+, press 2; For Gold; press 3; For Platinum, press 4)" << endl;
-    cin >> userinput;
-    } while ((userinput != 1) && (userinput != 2) && (userinput != 3) && (userinput != 4));
+    sf::Text title("New Member Registration", font, 30);
+    title.setFillColor(sf::Color::White);
+    title.setPosition(150, 50);
 
-    memberid = generateMemberId();
+    sf::Text instructions("Fill out the details below:", font, 20);
+    instructions.setFillColor(sf::Color::White);
+    instructions.setPosition(150, 120);
 
-    ofstream file("members.txt", ios::app);
+    sf::Text namePrompt("Enter your Name: ", font, 20);
+    namePrompt.setFillColor(sf::Color::White);
+    namePrompt.setPosition(150, 180);
 
-    file << name + "\n";
-    file << contactno + "\n";
-    file << membershipstartdate + "\n";
+    sf::Text contactPrompt("Enter Contact Number: ", font, 20);
+    contactPrompt.setFillColor(sf::Color::White);
+    contactPrompt.setPosition(150, 240);
 
-    if (userinput == 1)
-    {
-        file << "Silver \n";
-    }
-    else if (userinput == 2)
-    {
-        file << "Silver+ \n";
-    }
-    else if (userinput == 3)
-    {
-        file << "Gold \n";
-    }
-    else
-    {
-        file << "Platinum \n";
-    }
+    sf::Text membershipPrompt("Select Membership Type: ", font, 20);
+    membershipPrompt.setFillColor(sf::Color::White);
+    membershipPrompt.setPosition(150, 300);
 
-    file << "Active \n";
-    file << memberid + "\n";
-}
+    sf::RectangleShape silverButton(sf::Vector2f(200, 50));
+    silverButton.setFillColor(sf::Color::Blue);
+    silverButton.setPosition(150, 350);
 
-void memberTerminal()
-{
-    int userinput, userinput2;
-    string memberid;
-    do {
-        cout << "Enter 1 if you are an existing member or enter 2 if you are a new member";
-        cin >> userinput;
-        if ((userinput != 1) && (userinput != 2))
-        {
-            cout << "Enter 1 or 2 only!";
-        } 
-    } while ((userinput != 1) && (userinput != 2));
+    sf::Text silverText("Silver ($10)", font, 15);
+    silverText.setFillColor(sf::Color::White);
+    silverText.setPosition(170, 365);
 
-    if (userinput == 2)
-    {
-        newRegistration();
-    }
-    else
-    {
-        do {
-            cout << "Press 1 to check in or press 2 to check out: ";
-            cin >> userinput2;
-            if ((userinput2 != 1) && (userinput2 != 2))
-            {
-                cout << "Enter 1 or 2 only!";
-            } 
-        } while ((userinput2 != 1) && (userinput2 != 2));
+    sf::RectangleShape goldButton(sf::Vector2f(200, 50));
+    goldButton.setFillColor(sf::Color::Green);
+    goldButton.setPosition(400, 350);
 
-        cout << "Enter your Member ID: ";
-        cin >> memberid;
-        
-        if (userinput2 == 1)
-        {
-            memberCheckIn(memberid);
-        }
-        else
-        {
-            memberCheckOut(memberid);
-        }
-    }
-}
+    sf::Text goldText("Gold ($30)", font, 15);
+    goldText.setFillColor(sf::Color::White);
+    goldText.setPosition(420, 365);
 
-void managerTerminal()
-{
-    int actioninput;
-    int attempts = 1;
-    string password;
-    string correctpassword = "FitnessGym123";
-    bool access = false;
-    
-    cout << "Enter password to access Manager terminal. You only have four tries to enter correct password." << endl;
-    do {
-    cout << "Password: ";
-    cin >> password;
-    if (password == correctpassword)
-    {
-        access = true;
-        cout << "Access Granted!";
-    }
-    else
-    {
-        cout << "Wrong password. Please Try again!";
-    }
-    } while (attempts <= 4 && !access);
+    // User input variables
+    string enteredName, enteredContact;
+    int membershipChoice = 0;
 
-    if (!access)
-    {
-        cout << "Access Denied. " << endl << "Restarting the system........" << endl << endl << endl;
-    }
-    else
-    {
-        cout << "Select any action you want from the list below: " << endl;
-        cout << "   1, Update Member Information" << endl;
-        cout << "   2, Search Member Details" << endl;
-        cout << "   3, Report Generation" << endl;
-
-        do {
-            cout << "Enter 1 to Update Member Information; Enter 2 to Search for Member Details; Enter 3 to Generate Report: ";
-            cin >> actioninput;
-            if ((actioninput != 1) && (actioninput != 2) && (actioninput != 3))
-            {
-                cout << "Enter 1 or 2 or 3 only" << endl;
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
             }
-        } while ((actioninput != 1) && (actioninput != 2) && (actioninput != 3));
+            if (event.type == sf::Event::MouseButtonPressed) {
+                if (silverButton.getGlobalBounds().contains(window.mapPixelToCoords(sf::Mouse::getPosition(window)))) {
+                    membershipChoice = 1;
+                    membershiptype = "Silver";
+                    registrationComplete = true;
+                }
+                if (goldButton.getGlobalBounds().contains(window.mapPixelToCoords(sf::Mouse::getPosition(window)))) {
+                    membershipChoice = 2;
+                    membershiptype = "Gold";
+                    registrationComplete = true;
+                }
+            }
+        }
 
-        if (actioninput == 2)
-        {
-            string memberid;
+        // UI Rendering
+        window.clear();
+        window.draw(title);
+        window.draw(instructions);
+        window.draw(namePrompt);
+        window.draw(contactPrompt);
+        window.draw(membershipPrompt);
+        window.draw(silverButton);
+        window.draw(silverText);
+        window.draw(goldButton);
+        window.draw(goldText);
+        window.display();
 
-            cout << "Enter the ID of member you want to search the information from: ";
-            cin >> memberid;
-
-            searchMember(memberid);
+        if (registrationComplete) {
+            memberid = generateMemberId();
+            ofstream file("members.txt", ios::app);
+            file << enteredName + "\n";
+            file << enteredContact + "\n";
+            file << membershipstartdate + "\n";
+            file << membershiptype + "\n";
+            file << "Active\n";
+            file << memberid + "\n";
+            file.close();
+            cout << "Registration Successful!" << endl;
+            break;
         }
     }
 }
 
-int main()
-{
-    int userinput, userinput2;
-    cout << "Welcome to Gym Management System!" << endl;
-    do {
-        cout << "Enter 1 if you are a Member or enter 2 if you are a Manager: ";
-        cin >> userinput;
-        if ((userinput != 1) && (userinput != 2))
-        {
-            cout << "Enter 1 or 2 only!";
-        } } while ((userinput != 1) && (userinput != 2));
-    if (userinput == 1)
-    {
-        memberTerminal();
-    }
-    else
-    {
-        managerTerminal();
-    }
+// Member Terminal UI
+void memberTerminal(sf::RenderWindow &window, sf::Font &font) {
+    sf::Text title("Member Terminal", font, 30);
+    title.setFillColor(sf::Color::White);
+    title.setPosition(200, 50);
 
+    sf::RectangleShape registerButton(sf::Vector2f(200, 50));
+    registerButton.setFillColor(sf::Color::Blue);
+    registerButton.setPosition(200, 200);
+
+    sf::Text registerText("New Registration", font, 20);
+    registerText.setFillColor(sf::Color::White);
+    registerText.setPosition(220, 215);
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+            if (event.type == sf::Event::MouseButtonPressed) {
+                if (registerButton.getGlobalBounds().contains(window.mapPixelToCoords(sf::Mouse::getPosition(window)))) {
+                    newRegistration(window, font);
+                }
+            }
+        }
+
+        // UI Rendering
+        window.clear();
+        window.draw(title);
+        window.draw(registerButton);
+        window.draw(registerText);
+        window.display();
+    }
+}
+
+// Main Program UI
+void gymManagementSystem() {
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Gym Management System");
+    sf::Font font;
+    font.loadFromFile("arial.ttf");
+
+    sf::Text welcomeText("Welcome to Gym Management System", font, 30);
+    welcomeText.setFillColor(sf::Color::White);
+    welcomeText.setPosition(100, 50);
+
+    sf::RectangleShape memberButton(sf::Vector2f(200, 50));
+    memberButton.setFillColor(sf::Color::Blue);
+    memberButton.setPosition(150, 200);
+
+    sf::Text memberText("Member Terminal", font, 20);
+    memberText.setFillColor(sf::Color::White);
+    memberText.setPosition(170, 215);
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+            if (event.type == sf::Event::MouseButtonPressed) {
+                if (memberButton.getGlobalBounds().contains(window.mapPixelToCoords(sf::Mouse::getPosition(window)))) {
+                    memberTerminal(window, font);
+                }
+            }
+        }
+
+        // UI Rendering
+        window.clear();
+        window.draw(welcomeText);
+        window.draw(memberButton);
+        window.draw(memberText);
+        window.display();
+    }
+}
+
+int main() {
+    gymManagementSystem();
     return 0;
 }
